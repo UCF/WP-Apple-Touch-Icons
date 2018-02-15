1f5218783fdb1e01940e362a68e4228fbc54d13b
Jim Barnes <james.barnes@ucf.edu>
2018-02-15 16:22:47 -0500

    Corrected typo in main file

--- wp-apple-touch-icons.php
+++ wp-apple-touch-icons.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Apple Touch Icons
 Description: Provides the ability to upload a separate apple-touch-icon image, instead of using the site's favicon. Also allows for per page uploads of apple-icon-image.
-Verion: 1.0.0
+Version: 1.0.0
 Author: UCF Web Communications
 License: GPL3
 License URI: https://www.gnu.org/licenses/gpl.html

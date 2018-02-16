76ecc1cd6e2d50c40dc1443b44a91ae80c906f8b
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-02-16 09:07:21 -0500

    Updated whitespace

--- admin/wp-apple-touch-icons-admin.php
+++ admin/wp-apple-touch-icons-admin.php
@@ -3,18 +3,18 @@
  * Handles Plugin Options/Customizer Options
  **/
 if ( ! class_exists( 'WP_ATI_Admin' ) ) {
-    class WP_ATI_Admin {
-        public static
-            $size_array = array(
-                array( 'width' => 120, 'height' => 120, 'crop' => false ),
-                array( 'width' => 152, 'height' => 152, 'crop' => false ),
-                array( 'width' => 167, 'height' => 167, 'crop' => false ),
-                array( 'width' => 180, 'height' => 180, 'crop' => false )
+	class WP_ATI_Admin {
+		public static
+			$size_array = array(
+				array( 'width' => 120, 'height' => 120, 'crop' => false ),
+				array( 'width' => 152, 'height' => 152, 'crop' => false ),
+				array( 'width' => 167, 'height' => 167, 'crop' => false ),
+				array( 'width' => 180, 'height' => 180, 'crop' => false )
 			),
 			$primary_size = 180;
 
-        private static
-            $option_prefix = 'wp_ati_';
+		private static
+			$option_prefix = 'wp_ati_';
 
 		/**
 		 * Adds all image sizes. Used before images are resized.
@@ -56,51 +56,51 @@ if ( ! class_exists( 'WP_ATI_Admin' ) ) {
 			remove_image_size( $name );
 		}
 
-        /**
-         * Adds the Touch Icons section to the customizer
-         * @param WP_Customizer $wp_customize
-         */
-        public static function add_customizer_section( $wp_customize ) {
-            $wp_customize->add_section(
-                self::$option_prefix . 'general_section',
-                array(
-                    'title' => 'Apple Touch Icons'
-                )
-            );
-        }
-
-        /**
-         * Adds the apple touch icon settings and control
-         * @param WP_Customizer $wp_customize
-         */
-        public static function add_customizer_controls( $wp_customize ) {
-            $wp_customize->add_setting(
-                self::$option_prefix . 'apple_touch_icon',
-                array(
-                    'type' => 'option'
-                )
-            );
-
-            $wp_customize->add_control(
-                new WP_Customize_Image_Control(
-                    $wp_customize,
-                    self::$option_prefix . 'apple_touch_icon',
-                    array(
-                        'label'       => 'Apple Touch Icon',
-                        'description' => 'Icon that will be used when Apple user bookmark pages from this site.',
-                        'section'     => self::$option_prefix . 'general_section'
-                    )
-                )
-            );
+		/**
+		 * Adds the Touch Icons section to the customizer
+		 * @param WP_Customizer $wp_customize
+		 */
+		public static function add_customizer_section( $wp_customize ) {
+			$wp_customize->add_section(
+				self::$option_prefix . 'general_section',
+				array(
+					'title' => 'Apple Touch Icons'
+				)
+			);
 		}
 
 		/**
-         * Provides the value of the option after it's been saved
-         * @param mixed $old_value The old value
-         * @param mixed $new_value The new value
-         * @return mixed The new value modified
-         */
-        public static function save_apple_touch_icon( $old_value, $new_value ) {
+		 * Adds the apple touch icon settings and control
+		 * @param WP_Customizer $wp_customize
+		 */
+		public static function add_customizer_controls( $wp_customize ) {
+			$wp_customize->add_setting(
+				self::$option_prefix . 'apple_touch_icon',
+				array(
+					'type' => 'option'
+				)
+			);
+
+			$wp_customize->add_control(
+				new WP_Customize_Image_Control(
+					$wp_customize,
+					self::$option_prefix . 'apple_touch_icon',
+					array(
+						'label'       => 'Apple Touch Icon',
+						'description' => 'Icon that will be used when Apple user bookmark pages from this site.',
+						'section'     => self::$option_prefix . 'general_section'
+					)
+				)
+			);
+		}
+
+		/**
+		 * Provides the value of the option after it's been saved
+		 * @param mixed $old_value The old value
+		 * @param mixed $new_value The new value
+		 * @return mixed The new value modified
+		 */
+		public static function save_apple_touch_icon( $old_value, $new_value ) {
 			self::add_image_sizes();
 
 			$icon = new WP_ATI_Icons( $new_value, false );
@@ -108,6 +108,6 @@ if ( ! class_exists( 'WP_ATI_Admin' ) ) {
 			wp_update_attachment_metadata( $icon->attachment_id, $data );
 
 			self::remove_image_sizes();
-        }
-    }
+		}
+	}
 }
